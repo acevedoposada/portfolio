c0583d93a7c34ce867467be3dfb3c407ecafa88c
Cristian David Acevedo Posada <cacevedo@serempre.com>
2022-10-02 23:58:01 -0500

    feat(auth): Add credentials

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,26 +1,29 @@
-import admin from 'firebase-admin';
-import {
-  initializeApp as initializeAdminApp,
-  applicationDefault,
-} from 'firebase-admin/app';
-import { initializeApp } from 'firebase/app';
+import admin from "firebase-admin";
+import { initializeApp as initializeAdminApp } from "firebase-admin/app";
+import { initializeApp } from "firebase/app";
 
-require('dotenv').config();
+const { privateKey } = JSON.parse(
+  process.env.GOOGLE_PRIVATE_KEY || "{ privateKey: null }"
+);
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBCKUpi4ZyzpM7Xp7sH0o-nUW8gomyGWDA',
-  authDomain: 'portfolio-david-acevedo.firebaseapp.com',
-  projectId: 'portfolio-david-acevedo',
-  storageBucket: 'portfolio-david-acevedo.appspot.com',
-  messagingSenderId: '524541580212',
-  appId: '1:524541580212:web:63b2acb4d1cdc37a0598e1',
-  measurementId: 'G-0F0L5ZMEQG',
+  apiKey: "AIzaSyBCKUpi4ZyzpM7Xp7sH0o-nUW8gomyGWDA",
+  authDomain: "portfolio-david-acevedo.firebaseapp.com",
+  projectId: "portfolio-david-acevedo",
+  storageBucket: "portfolio-david-acevedo.appspot.com",
+  messagingSenderId: "524541580212",
+  appId: "1:524541580212:web:63b2acb4d1cdc37a0598e1",
+  measurementId: "G-0F0L5ZMEQG",
 };
 
 if (!admin.apps.length) {
   initializeAdminApp({
-    credential: applicationDefault(),
-    databaseURL: 'https://portfolio-david-acevedo.firebaseio.com',
+    credential: admin.credential.cert({
+      projectId: process.env.GOOGLE_PROJECTID,
+      privateKey,
+      clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    }),
+    databaseURL: "https://portfolio-david-acevedo.firebaseio.com",
   });
 }
 
