c5b9edc814cc5c9f47444642190bc6e472fa32e7
Cristian David Acevedo Posada <cacevedo@serempre.com>
2022-10-03 00:28:07 -0500

    feat(blog): Activate access to bd

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,40 +1,37 @@
 import type { LinksFunction } from "@remix-run/node";
-// import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 
-import { projectId } from "~/utils/db.server";
+import { db } from "~/utils/db.server";
 
 import blogStyles from "~/styles/pages/blog.css";
-
-console.log(process.env);
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: blogStyles }];
 };
 
-// export async function getPosts() {
-//   const querySnapshot = await db.collection("posts").get();
-//   const data: any[] = [];
-//   querySnapshot.forEach((doc) => {
-//     data.push({ ...doc.data(), id: doc.id });
-//   });
+export async function getPosts() {
+  const querySnapshot = await db.collection("posts").get();
+  const data: any[] = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ ...doc.data(), id: doc.id });
+  });
 
-//   return data;
-// }
+  return data;
+}
 
-// export const loader = () => {
-//   return getPosts();
-// };
+export const loader = () => {
+  return getPosts();
+};
 
 export default function Blog() {
-  // const data = useLoaderData();
+  const data = useLoaderData();
 
   return (
     <div>
       <h1 className="text-[56px]">Blog</h1>
-      {/* {data.map((element: any) => (
+      {data.map((element: any) => (
         <div key={element.id}>{element.title}</div>
-      ))} */}
-      {projectId}
+      ))}
     </div>
   );
 }
